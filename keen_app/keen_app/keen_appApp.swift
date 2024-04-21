5e114e57661b6a0fb205518bffeef35f2d6bd20f
Jennifer Tan <jennifermaria.tan@gmail.com>
2024-04-21 19:18:54 +1000

    Skeleton for iOS app in XCode

---
+++ keen_app/keen_app/keen_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  keen_appApp.swift
+//  keen_app
+//
+//  Created by Jennifer Tan on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct keen_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
